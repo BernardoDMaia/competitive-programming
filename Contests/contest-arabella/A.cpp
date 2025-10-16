1d294d35dd57398df0649146782d20a6ca507fc7
Bernardo <bernardodomingosmaia@gmail.com>
2025-10-16 13:28:28 +0000

    solve(contest): A

---
+++ Contests/contest-arabella/A.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0);
+    int n, k; cin >> n >> k;
+    cout << n*k << endl;
+}
