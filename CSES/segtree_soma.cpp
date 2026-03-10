983b40030a408081223780afdc1089a92e3141b3
Bernardo <bernardodomingosmaia@gmail.com>
2026-03-10 23:22:56 +0000

    solve(cses): segtree_soma

---
+++ CSES/segtree_soma.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f first
+#define s second 
+typedef vector<int>vi; 
+typedef pair<int,int>pi;
+
+const int MAX = 2e5 + 5;
+const int INF = 0;
+
+ll v[MAX];
+ll seg[MAX*4];
+
+ll build(int ind, int low, int high){
+    if(low==high) return seg[ind] = v[low];
+    int mid = (low+high)/2;
+    return seg[ind] = build(2*ind, low, mid) + build(2*ind+1, mid+1, high);
+}
+
+ll query(int ind, int low, int high, int l, int r){
+    if(r < low || high < l) return INF;
+    if(l <= low && high <= r) return seg[ind];
+    int mid = (low+high)/2;
+    return query(2*ind, low, mid, l, r) + query(2*ind+1, mid+1, high, l, r);
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);
+    
+    int n, q; cin >> n >> q;
+    for(int i = 0; i<n; i++) cin >> v[i];
+
+    build(1, 0, n-1);
+
+    while(q--){
+        int a,b; cin >> a >> b;
+        a--;b--;
+        cout << query(1, 0, n-1, a, b) << endl;
+    }
+
+}
