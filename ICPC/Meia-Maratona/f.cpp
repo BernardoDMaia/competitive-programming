b3849f14c8359107135d3b1e6ff78c9cdd268b3a
Bernardo <bernardodomingosmaia@gmail.com>
2026-03-24 00:33:46 +0000

    solve(icpc): f

---
+++ ICPC/Meia-Maratona/f.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll= long long;
+using vi = vector<int>;
+using vll =vector<ll>;
+using vpii =vector<pair<int,int>>;
+#define FOR(i,a,b) for(int i=a;i<(b);i++)
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define sz(x) (int)x.size()
+
+void solve(){
+    int n; cin >> n;
+    int c0, c1, c2; cin >> c0 >> c1 >> c2;
+    int k0, k1, k2; cin >> k0 >> k1 >> k2;
+    vi a(n);
+    vll ans(n);
+
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
