72e56f09d08f898c18ff3d458c8823e92812d808
Bernardo <bernardodomingosmaia@gmail.com>
2026-03-30 23:06:27 +0000

    solve(icpc): a

---
+++ ICPC/Maratona-Mineira/a.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll= long long;
+using vi = vector<int>;
+using vll =vector<ll>;
+using vpii =vector<pair<int,int>>;
+#define FOR(i,a,b) for(int i=a;i<(b);i++)
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define sz(x) (int)x.size()
+
+void solve(){
+    int y; cin >> y;
+    cout << 2024-(2*(2024-y)) << endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+}
