9d4cf3af0f70d559d18f76ce26e85c4e81ca6c71
Bernardo <bernardodomingosmaia@gmail.com>
2026-04-27 23:04:31 +0000

    solve(contest): j

---
+++ Contests/contest-russia/j.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i,a,b) for(int i = (a); i <(b); i++)
+#define pb push_back
+
+using vi = vector<int>;
+using ll = long long;
+using vl = vector<ll>;
+using pii = pair<int,int>;
+
+#define deb(x) cerr << #x << " = " << x << "\n";
+
+void solve(){
+    
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t=1;
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
