1126085085fabdf17b08c0fd84d3476ad3186d2f
Bernardo <bernardodomingosmaia@gmail.com>
2026-04-29 00:42:53 +0000

    solve(atcoder): e

---
+++ AtCoder/434/E.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll= long long;
+using vi = vector<int>;
+using vll =vector<ll>;
+using vc =vector<char>;
+using vpii =vector<pair<int,int>>;
+using pii = pair<int,int>;
+#define FOR(i,a,b) for(int i=a;i<(b);i++)
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define sz(x) (int)x.size()
+
+
+void solve(){
+    int n; cin >> n;
+    vector<vi>graph(n);
+    map<int,int>coord;
+    
+    FOR(i,0,n){
+        int x,y; cin >> x >> y;
+        coord[x+y] = ;
+        coord[x-y] = ;
+    }
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    solve();
+}
