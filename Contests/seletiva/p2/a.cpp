f1303bb83c6acb067eb5568704103da891dcabc3
Bernardo <bernardodomingosmaia@gmail.com>
2026-05-16 14:33:28 +0000

    solve(contest): a

---
+++ Contests/seletiva/p2/a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long ;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int,int>;
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define all(x) x.begin() , x.end()
+#define pb push_back
+
+void solve(){
+    int n; cin >> n;
+    vi mx(n+1), sufmx(n+1), mn(n+1), sufmn(n+1);
+    FOR(i,0,n){
+        char c; cin >> c;
+        if(c == '+'){
+            int u, x; cin >> u >> x;
+
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
