40f7c63e5c27ca373888e0cadb182cc68739dc67
Bernardo <bernardodomingosmaia@gmail.com>
2026-05-23 21:04:44 +0000

    solve(contest): a

---
+++ Contests/mfp/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long ;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int,int>;
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define all(x) x.begin() , x.end()
+#define pb push_back
+
+void solve(){
+    int x, y; cin >> x >> y;
+    cout << x*y << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    int t = 1; 
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+}
